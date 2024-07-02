bcd3d051d9970dc57f646f09181e11987fba1f05
Jhojan Grisales <redjhojan0319@gmail.com>
2024-07-01 23:52:54 -0500

    Inicio de estructura del todo app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" bg-black">
+        <h2 className="text-center text-4xl text-white font-bold">Todo App</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/data.ts
@@ -0,0 +1,8 @@
+import { Category } from "../types";
+
+export const categories: Category[] = [
+
+    { id: '1', name: 'Trabajo' },
+    { id: '2', name: 'Hogar' },
+    { id: '3', name: 'Social' }
+]
